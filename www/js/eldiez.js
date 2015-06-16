d9b3ae3f831a32fd8748ce0f83744e03ad2e7966
JossueG <jossueg@holagod.com>
2015-06-16 17:28:18 -0500

    correcciones del head status

--- www/js/eldiez.js
+++ www/js/eldiez.js
@@ -1,6 +1,6 @@
 $(document).on("ready", switchPagina);
 function todasLasPaginas(url){
-    StatusBar.overlaysWebView(false);
+    //StatusBar.overlaysWebView(false);
 
     //cargar cualquier pagina desde el borde superior
 	$('html, body').scrollTop(0);
