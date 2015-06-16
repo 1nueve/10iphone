af30b69a59724d950a6695aba34dda3f66b20e10
JossueG <jossueg@holagod.com>
2015-06-16 18:28:57 -0500

    agregando barra de status

--- www/js/eldiez.js
+++ www/js/eldiez.js
@@ -2,7 +2,7 @@ $(document).on("ready", switchPagina);
 function todasLasPaginas(url){
    if(window.StatusBar) {
     // org.apache.cordova.statusbar required
-        StatusBar.overlaysWebView(false);
+        StatusBar.backgroundColorByHexString("#1e1e1e");
         StatusBar.styleDefault();
     }
 
