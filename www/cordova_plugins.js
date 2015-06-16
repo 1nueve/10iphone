6add4a3a536d4cda4b31c21960d712a6c7b69464
JossueG <jossueg@holagod.com>
2015-06-16 17:13:38 -0500

    modificando el status bar

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
